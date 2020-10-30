7950551dc31be19f99f733d0e006f31e1883700e
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-30 13:12:50 -0300

    feat: include codegen

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,11 @@
-import 'graphql-import-node';
 import { makeExecutableSchema } from 'graphql-tools';
+import path from 'path';
 import { GraphQLSchema } from 'graphql';
-import * as typeDefs from './schema/schema.graphql';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
 import resolvers from './resolvers';
+
+const typeDefs = mergeTypeDefs(loadFilesSync(path.join(__dirname, 'schemas/**/*.graphql')));
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
